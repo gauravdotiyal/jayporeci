dd9945e95806e6e7675f2aa1a11ec4884f05e2d4
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2025-11-19 23:03:24 +0530

    Change the cicd

--- cicd/cicd.py
+++ cicd/cicd.py
@@ -1,6 +1,6 @@
 from jaypore_ci import jci
 
 with jci.Pipeline(image="python:3.11") as p:
-    p.job("Black",  ["python", "-c", "print(1+1)"])
+    p.job("Black",  ["python", "-c", "print(1+2)"])
     # p.job("Pylint", ["bash", "-c", "pip install pylint && pylint ."])
     # p.job("PyTest", ["bash", "-c", "pip install pytest && pytest"])
