5b745768eb09a6b673a3b1e4a8027e3e5705b418
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2025-11-19 17:02:01 +0530

    New changes

--- cicd/cicd.py
+++ cicd/cicd.py
@@ -1,6 +1,4 @@
 from jaypore_ci import jci
 
-with jci.Pipeline(image='mydocker/image') as p:
+with jci.Pipeline() as p:
     p.job("Black", "black --check .")
-    p.job("Pylint", "pylint mycode/ tests/")
-    p.job("PyTest", "pytest tests/")
