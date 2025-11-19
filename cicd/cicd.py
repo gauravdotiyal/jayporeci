b2b3c3d958fe5b5fcf90020bc6dc3535230bee22
Gaurav Dotiyal <gauravdotiyal33@gmail.com>
2025-11-19 14:06:30 +0530

    New changes

--- cicd/cicd.py
+++ cicd/cicd.py
@@ -1,4 +1,6 @@
 from jaypore_ci import jci
 
-with jci.Pipeline() as p:
-    p.job("Black", "black --check .")
+with jci.Pipeline(image="python:3.11") as p:
+    p.job("Black",  ["bash", "-c", "pip install black && black --check ."])
+    p.job("Pylint", ["bash", "-c", "pip install pylint && pylint ."])
+    p.job("PyTest", ["bash", "-c", "pip install pytest && pytest"])
